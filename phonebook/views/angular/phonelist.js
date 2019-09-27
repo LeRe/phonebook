d00af656066315d0b491400ad06116b891703f25
LeRe <levchenko.rp@gmail.com>
2019-09-27 16:39:02 +0300

    -

--- phonebook/views/angular/phonelist.js
+++ phonebook/views/angular/phonelist.js
@@ -1,8 +1,12 @@
 var phoneApp = angular.module('phoneApp', []);
 phoneApp.controller('phoneListController', function phoneListController($scope, $http) {
     $http.get('/json/personlist').then((response) => { $scope.persons = response.data });
+    $scope.isEditModeEnable = false;
     $scope.openPerson = function (id) {
         $http.get('/json/person?id=' + id).then((response) => { $scope.curentPerson = response.data });
         $('#personModal').modal();
+    }
+    $scope.editModeChanged = function () {
+        
     }
 });
